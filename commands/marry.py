566364209625341af83fb8135ecc64b66c0f7377
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-30 15:41:56 +0300

    Added chatID to db

--- commands/marry.py
+++ commands/marry.py
@@ -6,14 +6,15 @@ from datetime import datetime, timezone
 database = db.Database()
 
 class User:
-    def __init__(self, userID):
+    def __init__(self, userID, chatID):
         self.id = userID
-        self.firstname = database.getFirstname(self.id)
-        self.marriedTo = database.getUserMarriedTo(self.id)
-        self.marriedAt = database.getUserMarriedAt(self.id)
-        self.petID = database.getPet(self.id)
+        self.chatID = chatID
+        self.firstname = database.getFirstname(self.id, self.chatID)
+        self.marriedTo = database.getUserMarriedTo(self.id, self.chatID)
+        self.marriedAt = database.getUserMarriedAt(self.id, self.chatID)
+        self.petID = database.getPet(self.id, self.chatID)
     
-    def updateUser(self, firstname=None, marriedTo=None, marriedAt = None, petID=None):
+    def updateUser(self, firstname=None, marriedTo=None, marriedAt=None, petID=None):
         infoToUpdate = {
             "firstname": firstname,
             "marriedTo": marriedTo,
@@ -22,13 +23,13 @@ class User:
         }
         for entry in infoToUpdate:
             if entry == "firstname" and infoToUpdate["firstname"] is not None:
-                database.updateUserFirstname(self.id, firstname)
+                database.updateUserFirstname(self.id, self.chatID, firstname)
             elif entry == "marriedTo" and infoToUpdate["marriedTo"] is not None:
-                database.updateUserMarriedTo(self.id, marriedTo)
+                database.updateUserMarriedTo(self.id, self.chatID, marriedTo)
             elif entry == "marriedAt" and infoToUpdate["marriedAt"] is not None:
-                database.updateUserMarriedAt(self.id, marriedAt)
+                database.updateUserMarriedAt(self.id, self.chatID, marriedAt)
             elif entry == "petID" and infoToUpdate["petID"] is not None:
-                database.updateUserPet(self.id, petID)
+                database.updateUserPet(self.id, self.chatID, petID)
 
 async def marry(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chatID = update.effective_chat.id
@@ -41,14 +42,13 @@ async def marry(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     proposingUser = update.effective_user
-    if not database.lookUpUser(proposingUser.id):
-        database.insertNewUser(proposingUser.id, proposingUser.first_name)
+    if not database.lookUpUser(proposingUser.id, chatID):
+        database.insertNewUser(proposingUser.id, chatID, proposingUser.first_name)
 
-    proposingUserObj = User(proposingUser.id)
+    proposingUserObj = User(proposingUser.id, chatID)
 
-    # If user A is already married
     if proposingUserObj.marriedTo is not None:
-        marriedToObj = User(proposingUserObj.marriedTo)
+        marriedToObj = User(proposingUserObj.marriedTo, chatID)
         await context.bot.send_message(
             chat_id=chatID,
             text=f'<a href="tg://user?id={proposingUserObj.id}">{proposingUserObj.firstname}</a>, you are already married to <a href="tg://user?id={marriedToObj.id}">{marriedToObj.firstname}</a>!',
@@ -56,23 +56,19 @@ async def marry(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         return
 
-    # Get who user A is proposing to (user B)
     message = update.effective_message
-    proposingToUsername = None
     proposingToUser = None
     for entry in message.entities:
         if entry.type == "mention":
             proposingToUsername = message.parse_entity(entry)
             proposingToUser = await context.bot.get_chat(proposingToUsername)
-            if not database.lookUpUser(proposingToUser.id):
-                database.insertNewUser(proposingToUser.id, proposingToUser.first_name)
+            if not database.lookUpUser(proposingToUser.id, chatID):
+                database.insertNewUser(proposingToUser.id, chatID, proposingToUser.first_name)
             break
         elif entry.type == "text_mention":
             proposingToUser = entry.user
-            proposingToFirstname = proposingToUser.first_name
-            proposingToID = proposingToUser.id
-            if not database.lookUpUser(proposingToID):
-                database.insertNewUser(proposingToID, proposingToFirstname)
+            if not database.lookUpUser(proposingToUser.id, chatID):
+                database.insertNewUser(proposingToUser.id, chatID, proposingToUser.first_name)
             break
 
     if proposingToUser is None:
@@ -82,31 +78,29 @@ async def marry(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         return
 
-    proposingToUserObj = User(proposingToUser.id)
+    proposingToUserObj = User(proposingToUser.id, chatID)
 
-    # If user B is already married
     if proposingToUserObj.marriedTo is not None:
-        marriedToObj = User(proposingToUserObj.marriedTo)
+        marriedToObj = User(proposingToUserObj.marriedTo, chatID)
         await context.bot.send_message(
             chat_id=chatID,
             text=f'<a href="tg://user?id={proposingToUserObj.id}">{proposingToUserObj.firstname}</a> is already married to <a href="tg://user?id={marriedToObj.id}">{marriedToObj.firstname}</a>!',
             parse_mode="HTML"
         )
         return
-    
-    # Both users aren't married, send a proposal to user B from user A
+
     keyboard = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("💍Yes", callback_data = f"proposalYes:{proposingUserObj.id}:{proposingToUserObj.id}"),
-            InlineKeyboardButton("💔No", callback_data = f"proposalNo:{proposingUserObj.id}:{proposingToUserObj.id}")
+            InlineKeyboardButton("💍Yes", callback_data=f"proposalYes:{chatID}:{proposingUserObj.id}:{proposingToUserObj.id}"),
+            InlineKeyboardButton("💔No", callback_data=f"proposalNo:{chatID}:{proposingUserObj.id}:{proposingToUserObj.id}")
         ]
     ])
 
     await context.bot.send_message(
-        chat_id = update.effective_chat.id,
-        text = f'<a href="tg://user?id={proposingUserObj.id}">{proposingUserObj.firstname}</a> is proposing to <a href="tg://user?id={proposingToUserObj.id}">{proposingToUserObj.firstname}</a>\nWhat are they going to say?',
-        reply_markup = keyboard,
-        parse_mode = "HTML"
+        chat_id=chatID,
+        text=f'<a href="tg://user?id={proposingUserObj.id}">{proposingUserObj.firstname}</a> is proposing to <a href="tg://user?id={proposingToUserObj.id}">{proposingToUserObj.firstname}</a>\nWhat are they going to say?',
+        reply_markup=keyboard,
+        parse_mode="HTML"
     )
 
 async def marry_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -115,48 +109,49 @@ async def marry_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.answer()
         return
 
-    action, proposingUserID, proposingToUserID = query.data.split(":")
+    action, chatID, proposingUserID, proposingToUserID = query.data.split(":")
+    chatID = int(chatID)
 
-    proposingUserObj = User(proposingUserID)
-    proposingToUserObj = User(proposingToUserID)
+    proposingUserObj = User(proposingUserID, chatID)
+    proposingToUserObj = User(proposingToUserID, chatID)
 
     if query.from_user.id != int(proposingToUserObj.id):
         await query.answer(
-            text = "This proposal is not for you!",
-            show_alert = True)
+            text="This proposal is not for you!",
+            show_alert=True)
     elif query.from_user.id == int(proposingToUserObj.id):
         if action == "proposalYes":
             await query.answer()
 
             time = datetime.now(timezone.utc).isoformat()
-            proposingUserObj.updateUser(marriedTo = int(proposingToUserObj.id), marriedAt = time)
-            proposingToUserObj.updateUser(marriedTo = int(proposingUserObj.id), marriedAt = time)
+            proposingUserObj.updateUser(marriedTo=int(proposingToUserObj.id), marriedAt=time)
+            proposingToUserObj.updateUser(marriedTo=int(proposingUserObj.id), marriedAt=time)
 
-            text = f'<a href="tg://user?id={proposingUserObj.id}">{proposingUserObj.firstname}</a> and <a href="tg://user?id={proposingToUserObj.id}">{proposingToUserObj.firstname}</a> are now married! Let\'s congratulate them!' 
-            
+            text = f'<a href="tg://user?id={proposingUserObj.id}">{proposingUserObj.firstname}</a> and <a href="tg://user?id={proposingToUserObj.id}">{proposingToUserObj.firstname}</a> are now married! Let\'s congratulate them!'
+
             keyboard = InlineKeyboardMarkup([
                 [
-                    InlineKeyboardButton("🎊🎉", callback_data = "nothing")
+                    InlineKeyboardButton("🎊🎉", callback_data="nothing")
                 ]
             ])
 
             await query.edit_message_reply_markup(reply_markup=keyboard)
 
             await context.bot.send_photo(
-                chat_id = update.effective_chat.id,
-                caption = text,
-                photo = open("resource/marriage/marriedCoupleOne.jpg", "rb"),
-                parse_mode = "HTML"
+                chat_id=chatID,
+                caption=text,
+                photo=open("resource/marriage/marriedCoupleOne.jpg", "rb"),
+                parse_mode="HTML"
             )
-        
+
         elif action == "proposalNo":
             await query.answer()
 
             text = "The proposal was declined 💔"
-            
+
             keyboard = InlineKeyboardMarkup([
                 [
-                    InlineKeyboardButton("💔", callback_data = "nothing")
+                    InlineKeyboardButton("💔", callback_data="nothing")
                 ]
             ])
 
--- db.py
+++ db.py
@@ -7,65 +7,71 @@ key = os.environ.get("SUPABASE_KEY")
 class Database:
     def __init__(self):
         self.supabase = create_client(url, key)
+
     def getClient(self):
         return self.supabase
     
-    def lookUpUser(self, userID):
+    def lookUpUser(self, userID, chatID):
         db = self.getClient()
-        response = db.table("users").select("*").eq("userID", int(userID)).execute()
+        response = db.table("users").select("*").eq("userID", int(userID)).eq("chatID", int(chatID)).execute()
         return bool(response.data)
     
-    def insertNewUser(self, userID, firstname):
+    def insertNewUser(self, userID, chatID, firstname):
         db = self.getClient()
         db.table("users").upsert({
             "userID": int(userID),
+            "chatID": int(chatID),
             "firstname": firstname
         }).execute()
 
-    def updateUserFirstname(self, userID, firstname):
+    def updateUserFirstname(self, userID, chatID, firstname):
         db = self.getClient()
         db.table("users").upsert({
             "userID": int(userID),
+            "chatID": int(chatID),
             "firstname": firstname
         }).execute()
 
-    def updateUserMarriedTo(self, userID, marriedTo):
+    def updateUserMarriedTo(self, userID, chatID, marriedTo):
         db = self.getClient()
         db.table("users").upsert({
             "userID": int(userID),
+            "chatID": int(chatID),
             "marriedTo": marriedTo
         }).execute()
 
-    def updateUserMarriedAt(self, userID, marriedAt):
+    def updateUserMarriedAt(self, userID, chatID, marriedAt):
         db = self.getClient()
         db.table("users").upsert({
             "userID": int(userID),
+            "chatID": int(chatID),
             "marriedAt": marriedAt
         }).execute()
 
-    def updateUserPet(self, userID, petID):
+    def updateUserPet(self, userID, chatID, petID):
         db = self.getClient()
         db.table("users").upsert({
-            "userID": userID,
+            "userID": int(userID),
+            "chatID": int(chatID),
             "petID": petID
         }).execute()
 
-    def getUserMarriedTo(self, userID):
+    def getUserMarriedTo(self, userID, chatID):
         db = self.getClient()
-        response = (db.table("users").select("*").eq("userID", int(userID)).execute()).data
+        response = (db.table("users").select("*").eq("userID", int(userID)).eq("chatID", int(chatID)).execute()).data
         return response[0]["marriedTo"]
     
-    def getUserMarriedAt(self, userID):
+    def getUserMarriedAt(self, userID, chatID):
         db = self.getClient()
-        response = (db.table("users").select("*").eq("userID", int(userID)).execute()).data
+        response = (db.table("users").select("*").eq("userID", int(userID)).eq("chatID", int(chatID)).execute()).data
         return response[0]["marriedAt"]
     
-    def getFirstname(self, userID):
+    def getFirstname(self, userID, chatID):
         db = self.getClient()
-        response = (db.table("users").select("*").eq("userID", int(userID)).execute()).data
+        response = (db.table("users").select("*").eq("userID", int(userID)).eq("chatID", int(chatID)).execute()).data
         return response[0]["firstname"]
     
-    def getPet(self, userID):
+    def getPet(self, userID, chatID):
         db = self.getClient()
-        response = (db.table("users").select("*").eq("userID", int(userID)).execute()).data
+        response = (db.table("users").select("*").eq("userID", int(userID)).eq("chatID", int(chatID)).execute()).data
         return response[0]["petID"]
