c00a67f846981a071a18d2bca31dbd68876ea216
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-30 03:56:23 +0300

    testing

--- handlers/messageHandler.py
+++ handlers/messageHandler.py
@@ -2,10 +2,20 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 async def processMessage(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chatID = update.effective_chat.id
+
     isGroupChat = update.effective_chat.type in ["group", "supergroup"]
     if not isGroupChat:
         context.bot.send_message(
-            chat_id = update.effective_chat.id,
+            chat_id = chatID,
             text = "Sorry, currently I only work in group chats."
         )
         return
+    
+    message = update.effective_message.text
+    if message == "test":
+        context.bot.send_message(
+            chat_id = chatID,
+            text = "Hiii, <bold>testing</bold> things.",
+            parse_mode = "HTML"
+        )
